2d4b94fd84f577cbb87f1b6e0a19101bb80c8eb7
Stephen Kofi Osei-Akoto <stephenosei74@hotmail.com>
2021-01-27 20:25:10 -0500

    created view and controller

---
+++ flasktaskr/project/_config.py
@@ -0,0 +1,13 @@
+import os
+
+# grab the folder where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'W%\xcc\xf5\xe6\x16\xec\xd4\xa0q{\x8c=\x88C%6~\x85\xd6Q\x07~O'
+
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
