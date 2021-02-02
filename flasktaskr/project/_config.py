0b15e19b322ba0e88bff96a39440c80713759b54
Stephen Kofi Osei-Akoto <stephenosei74@hotmail.com>
2021-02-01 21:08:51 -0500

    Converted SQL code to ORM using SQLAlchemy

--- flasktaskr/project/_config.py
+++ flasktaskr/project/_config.py
@@ -1,3 +1,5 @@
+# project/_config.py
+
 import os
 
 # grab the folder where this script lives
@@ -6,8 +8,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 SECRET_KEY = 'W%\xcc\xf5\xe6\x16\xec\xd4\xa0q{\x8c=\x88C%6~\x85\xd6Q\x07~O'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
