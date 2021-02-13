0a61e7b356220402f80156e7b0f8537d5b5e8967
Stephen Kofi Osei-Akoto <stephenosei74@hotmail.com>
2021-02-12 19:41:03 -0500

    Testing with Testing with fabric

---
+++ flasktaskr/fabfile.py
@@ -0,0 +1,25 @@
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
+def test():
+    local("nosetests -v")
+
+def commit():
+    message = input("Enter a git commit message: ")
+    local("git add . && git commit -am '{}'".format(message))
+
+
+def push():
+    local("git push origin master")
+
+def prepare():
+    test()
+    commit()
+    push()
+
+
+def test():
+    with settings(warn_only=True):
+        result = local("nosetests -v", capture=True)
+    if result.failed and not confirm("Tests failed. Continue?"):
+        abort("Aborted at user request.")
